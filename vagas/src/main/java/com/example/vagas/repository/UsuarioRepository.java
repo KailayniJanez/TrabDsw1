c7fe0e749cef25b94a1e871820045d1d853d20eb
Brenda Maia <brendaraquel80@gmail.com>
2025-07-14 07:23:33 -0300

    ajustando arquivo

--- vagas/src/main/java/com/example/vagas/repository/UsuarioRepository.java
+++ vagas/src/main/java/com/example/vagas/repository/UsuarioRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-    // Este método buscará um Usuario (ou suas subclasses: Empresa ou Profissional) pelo email
+    // buscando um Usuario (ou suas subclasses: Empresa ou Profissional) pelo email
     Optional<Usuario> findByEmail(String email);
 }
