c022fcc8ce421a8c46bbc08649a1db302959efdd
Brenda Maia <brendaraquel80@gmail.com>
2025-07-14 11:43:02 -0300

    atualizações

--- vagas/src/main/java/com/example/vagas/model/Profissional.java
+++ vagas/src/main/java/com/example/vagas/model/Profissional.java
@@ -1,61 +1,112 @@
 package com.example.vagas.model;
 
+
 import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.PrimaryKeyJoinColumn;
+
 import jakarta.persistence.Table;
+
 import java.time.LocalDate;
 
 
+
 @Entity
-@Table(name = "profissional") 
-@PrimaryKeyJoinColumn(name = "id") 
-public class Profissional extends Usuario { 
 
-    @Column(unique = true, nullable = false)
-    private String cpf;
+@Table(name = "profissional")
 
-    private String telefone;
-    private String sexo;
-    private LocalDate dataNascimento;
+@PrimaryKeyJoinColumn(name = "id")
 
-    public Profissional() {
-        super();
-        this.setRole("ROLE_PROFISSIONAL");
-    }
+public class Profissional extends Usuario {
 
-    public Profissional(String email, String senha, String nome, String cpf, String telefone, String sexo, LocalDate dataNascimento) {
-        super(email, senha, nome, "ROLE_PROFISSIONAL");
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.sexo = sexo;
-        this.dataNascimento = dataNascimento;
-    }
 
-    // Getters e Setters de Profissional
-    public String getCpf() { 
-        return cpf; 
-    }
-    public void setCpf(String cpf) { 
-        this.cpf = cpf; 
-    }
-    public String getTelefone() { 
-        return telefone; 
-    }
-    public void setTelefone(String telefone) { 
-        this.telefone = telefone; 
-    }
-    public String getSexo() { 
-        return sexo; 
-    }
-    public void setSexo(String sexo) { 
-        this.sexo = sexo; 
-    }
-    public LocalDate getDataNascimento() { 
-        return dataNascimento; 
-    }
-    public void setDataNascimento(LocalDate dataNascimento) { 
-        this.dataNascimento = dataNascimento; 
-    }
+@Column(unique = true, nullable = false)
+
+private String cpf;
+
+
+private String telefone;
+
+private String sexo;
+
+private LocalDate dataNascimento;
+
+
+public Profissional() {
+
+super();
+
+this.setRole("ROLE_PROFISSIONAL");
 
 }
+
+
+public Profissional(String email, String senha, String nome, String cpf, String telefone, String sexo, LocalDate dataNascimento) {
+
+super(email, senha, nome, "ROLE_PROFISSIONAL");
+
+this.cpf = cpf;
+
+this.telefone = telefone;
+
+this.sexo = sexo;
+
+this.dataNascimento = dataNascimento;
+
+}
+
+
+// Getters e Setters de Profissional
+
+public String getCpf() {
+
+return cpf;
+
+}
+
+public void setCpf(String cpf) {
+
+this.cpf = cpf;
+
+}
+
+public String getTelefone() {
+
+return telefone;
+
+}
+
+public void setTelefone(String telefone) {
+
+this.telefone = telefone;
+
+}
+
+public String getSexo() {
+
+return sexo;
+
+}
+
+public void setSexo(String sexo) {
+
+this.sexo = sexo;
+
+}
+
+public LocalDate getDataNascimento() {
+
+return dataNascimento;
+
+}
+
+public void setDataNascimento(LocalDate dataNascimento) {
+
+this.dataNascimento = dataNascimento;
+
+}
+
+
+} 
